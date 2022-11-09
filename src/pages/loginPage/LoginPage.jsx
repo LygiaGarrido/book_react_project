bd8893d7be6f9740bc1553aceed6e3961c80e564
Joao_Guima <jotam24@hotmail.com>
2022-11-09 19:29:38 +0000

    Implement navigate routes

--- src/pages/loginPage/LoginPage.jsx
+++ src/pages/loginPage/LoginPage.jsx
@@ -1,5 +1,7 @@
 import { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { Footer } from '../../components/Footer';
+import { Header } from '../../components/Header';
 import { UserContext } from '../../contexts/UserContext';
 
 function LoginPage() {
@@ -33,37 +35,41 @@ function LoginPage() {
     }
 
     return (
-        <div id='login'>
-            <form
-                id='card'
-                onSubmit={handleSubmit}
-            >
-                <div className='card-header'>
-                    <h1>Login</h1>
-                </div>
-                <div className='card-content'>
-                    <div className='card-content-area'>
-                        <label> E-mail:</label>
-                        <input
-                            type='text'
-                            name='username'
-                            required
-                        />
+        <>
+            <Header />
+            <div id='login'>
+                <form
+                    id='card'
+                    onSubmit={handleSubmit}
+                >
+                    <div className='card-header'>
+                        <h1>Login</h1>
                     </div>
-                    <div className='card-content-area'>
-                        <label> Password:</label>
-                        <input
-                            type='password'
-                            name='password'
-                            required
-                        />
+                    <div className='card-content'>
+                        <div className='card-content-area'>
+                            <label> E-mail:</label>
+                            <input
+                                type='text'
+                                name='username'
+                                required
+                            />
+                        </div>
+                        <div className='card-content-area'>
+                            <label> Password:</label>
+                            <input
+                                type='password'
+                                name='password'
+                                required
+                            />
+                        </div>
                     </div>
-                </div>
-                <div className='card-footer'>
-                    <button type='submit'>login</button>
-                </div>
-            </form>
-        </div>
+                    <div className='card-footer'>
+                        <button type='submit'>login</button>
+                    </div>
+                </form>
+            </div>
+            <Footer />
+        </>
     );
 }
 
