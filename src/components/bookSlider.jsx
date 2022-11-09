a20cf66fb1a1ae627050f1d00b6353dad3c06b0e
Joao_Guima <jotam24@hotmail.com>
2022-11-09 11:06:26 +0000

    implement bookSlider

---
+++ src/components/bookSlider.jsx
@@ -0,0 +1,64 @@
+import { useEffect } from 'react';
+import styled from 'styled-components';
+
+const Highlight_book = styled.div`
+    display: flex;
+    justify-content: center;
+    margin-left: 198px;
+    margin-right: 124px;
+    font-size: 30px;
+    color: #343434;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    border-radius: 9px;
+`;
+
+const Book_description = styled.div`
+    margin-top: 43px;
+    margin-bottom: 20px;
+    margin-right: 58px;
+    margin-left: 103px;
+`;
+
+const Book_cover = styled.div`
+    margin-top: 15px;
+    margin-bottom: 7px;
+    margin-right: 58px;
+    margin-left: 27px;
+    flex-wrap: wrap;
+`;
+
+function BookSlider(props) {
+    let currentBook = 0;
+
+    useEffect(() => {
+        setInterval(() => {
+            currentBook < 4 ? currentBook++ : (currentBook = 0);
+            props.setCurrent(currentBook);
+        }, 5000);
+    }, [currentBook]);
+
+    function handleClick() {
+        //TODO (insert link to bookpage)
+        console.log(props.bookID);
+    }
+
+    return (
+        <>
+            <Highlight_book onClick={handleClick}>
+                <Book_cover>
+                    <img
+                        height='199px'
+                        width='auto'
+                        src={props.bookCover}
+                        alt={props.bookTitle}
+                    />
+                </Book_cover>
+                <Book_description>
+                    <p>{props.bookDescription}</p>
+                </Book_description>
+            </Highlight_book>
+        </>
+    );
+}
+
+export { BookSlider };
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,49 @@
-import { Header } from "./Header";
-import { HeroSection } from "./HeroSection";
-import { Footer } from "./Footer";
+import { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import { Header } from '../components/Header';
+import { HeroSection } from '../components/HeroSection';
+import { Footer } from '../components/Footer';
+import { BookSlider } from '../components/bookSlider';
 
 function HomePage() {
-  return (
-    <>
-      <Header />
-      <HeroSection />
-      <Footer />
-    </>
-  );
+    const [books, setBooks] = useState([]);
+    const [currentBook, setCurrentBook] = useState(0);
+
+    useEffect(() => {
+        async function getAllBooks() {
+            const response = await fetch(
+                'https://ancient-temple-61124.herokuapp.com/api/book/'
+            );
+            const json = await response.json();
+            setBooks(json.data);
+        }
+
+        getAllBooks();
+    }, []);
+
+    const bookList = books.map((book) => {
+        return (
+            <BookSlider
+                bookTitle={book.title}
+                bookDescription={book.description}
+                bookCover={book.book_cover}
+                bookID={book.id}
+                setCurrent={setCurrentBook}
+            />
+        );
+    });
+
+    return (
+        <>
+            <Header />
+            <HeroSection />
+            <div className='book-list-div'>
+                <h3 className='book-container-title'>The week's best books</h3>
+                {bookList[currentBook]}
+            </div>
+            <Footer />
+        </>
+    );
 }
 
-export {HomePage};
+export { HomePage };
